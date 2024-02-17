8b7d56da9a38cda75b7e53ac36ea538dad3c83c0
irakli85 <iberidze38@gmail.com>
2024-02-17 17:35:24 +0400

    show more

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -10,7 +10,7 @@ export default async function Home({searchParams}:any) {
     manufacturer: searchParams.manufacturer || '',
     year: searchParams.year || 2022,
     fuel: searchParams.fuel || '',
-    limit: searchParams.limit || 12,
+    limit: searchParams.limit || 10,
     model: searchParams.model || ''
   })
 
@@ -38,6 +38,7 @@ export default async function Home({searchParams}:any) {
               <div className="home__cars-wrapper">
                 {allCars?.map((car) => <CarCard car={car}/>)}
               </div>
+              <ShowMore pageNumber = {(searchParams.limit || 10)/10} isNext={(searchParams.limit || 10) > allCars.length}/>
             </section>
           ) : (
             <div className="home__error-container">
--- components/index.ts
+++ components/index.ts
@@ -7,7 +7,8 @@ import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard"
 import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
 
 export {
-    Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails
+    Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer, CarCard, CarDetails, ShowMore
 }
