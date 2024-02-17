3c0a0c76bbea0c117bdd591cf9b12cbd3969616e
irakli85 <iberidze38@gmail.com>
2024-02-17 14:46:28 +0400

    search bar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 
 
-export default async function Home({searchParams}) {
+export default async function Home({searchParams}:any) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || '',
     year: searchParams.year || 2022,
