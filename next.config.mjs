ab7ec2311a8675f8d51c79a3ae8cd2578e4c3f1e
irakli85 <iberidze38@gmail.com>
2024-02-17 14:43:49 +0400

    search bar

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {    
     images: {
-        domains: ['cdn.imagin.studio']
-    }
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.imagin.studio',
+            pathname: '**',
+          },
+        ],
+      },
 };
 
 export default nextConfig;
